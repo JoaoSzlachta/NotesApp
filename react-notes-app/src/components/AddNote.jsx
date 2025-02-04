1d34e8f47a05b95dd9cb9cb96a0c82ac2953c473
JoaoSzlachta <jpbs2002@gmail.com>
2025-02-04 13:43:47 -0300

    chore: start new note feature

---
+++ react-notes-app/src/components/AddNote.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const AddNote = () => {
+    const [noteText, setNoteText] = useState('');
+
+    const handleChange = (e) => {
+        setNoteText(e.target.value);
+    };
+
+    const handleClick = () => {
+        
+    };
+
+    return (
+        <div className="note new">
+            <textarea 
+                rows='8'
+                cols='10'
+                placeholder="Type to add a note..."
+                onChange={handleChange()}
+                value={noteText}
+            ></textarea>
+            <div className="note-footer">
+                <small>200 remaing</small>
+                <button className="save" onClick={handleSaveClick()}>Save</button>
+            </div>
+        </div>
+    )
+}
+export default AddNote;
